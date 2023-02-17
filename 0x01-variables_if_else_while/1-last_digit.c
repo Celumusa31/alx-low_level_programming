c982a63ee1e665673554ddfa1845028b9f439d49
Celumusa31 <celumusamthombeni31@gmail.com>
2023-02-17 09:07:20 -0800

    Task 1 corrections

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -18,7 +18,7 @@ int main(void)
 
 	if (n > 5)
 	{
-		printf("Last digit of %d is and %d is greater than 5\n", n, last_digit);
+		printf("Last digit of %d is %d and is greater than 5\n", n, last_digit);
 	}
 	else if (n == 0)
 	{
