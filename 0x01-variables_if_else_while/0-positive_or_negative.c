d2fdeeb213e5daa3c3e9c9ba1e7d9b7ac70439c7
Celumusa31 <celumusamthombeni31@gmail.com>
2023-02-17 06:54:56 -0800

    Updating task 0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,9 +3,9 @@
 #include <stdio.h>
 
 /**
- * main:  Entry point
- *Describtion: program prints whether a number is positive or negative
- * return: 0
+ * main -Entry point
+ *
+ * return: Always 0 (Success)
  */
 int main(void)
 {
@@ -15,16 +15,15 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
 	{
-		printf("%d is positive\n", n);
+		printf("%d is positive", n);
 	}
 	else if (n == 0)
 	{
-		printf("%d is zero\n", n);
-	{
+		printf("%d is zero", n);
+	}
 	else
 	{
-		printf("%d is negative\n", n);
+		printf("%d is negative", n);
 	}
-
 	return (0);
 }
