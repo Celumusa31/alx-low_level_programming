70cabe37386ab161ceaf86f026500c0bbb339a10
Celumusa31 <celumusamthombeni31@gmail.com>
2023-02-21 12:24:33 -0800

    Fixing task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,9 +10,11 @@ void print_alphabet_x10(void)
 	char alphabet;
 
 	for (i = 0; i < 10; i++)
+	{
 		for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
-		
+		{
 			_putchar(alphabet);
-		
+		}
+	}
 		_putchar('\n');
 }
