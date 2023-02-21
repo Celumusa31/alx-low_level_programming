2ec293e29a1e39a79f60d1646d82497e2f9dcd32
Celumusa31 <celumusamthombeni31@gmail.com>
2023-02-21 11:12:27 -0800

    Betty

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,7 +5,8 @@
  *
  * Return: Always void.
  */
-void print_alphabet(void){
+void print_alphabet(void)
+{
 	char alphabet;
 	for(alphabet = 'a'; alphabet <= 'z'; alphabet++){
 		_putchar(alphabet);
