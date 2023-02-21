039fd29dd34b00819b5b9cc249ab71cfd8a6d85b
Celumusa31 <celumusamthombeni31@gmail.com>
2023-02-21 15:38:46 -0800

    Task 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * print_last_digit - Entry point
+ * @n: The character to be checked
+ * Return: The value of last digit (Success)
+ */
+int print_last_digit(int n)
+{
+	lastDigit = n % 10;
+	return (lastDigit);
+}
+	
