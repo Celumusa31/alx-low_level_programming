35d1e242a87c28c6eb93b190d30c7a567a1e8acf
Celumusa31 <celumusamthombeni31@gmail.com>
2023-02-21 10:45:34 -0800

    Update files again

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -5,6 +5,14 @@
  *
  * Return: Always 0.
  */
+void print_alphabet(void){
+	char alphabet;
+	for(alphabet = 'a'; alphabet <= 'z'; alphabet++){
+		_putchar(alphabet);
+	}
+	_putchar('\n');
+}
+
 int main(void)
 {
 	print_alphabet();
