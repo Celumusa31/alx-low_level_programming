9fc66eac53aeeba9423d9d92d3bb8259a2ea95fc
Celumusa31 <celumusamthombeni31@gmail.com>
2023-02-21 11:10:38 -0800

    Nkare nka loma ALX

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- * main - check the code
+ * print_alphabet - check the code
  *
- * Return: Always 0.
+ * Return: Always void.
  */
-void print_alphabet(){
+void print_alphabet(void){
 	char alphabet;
 	for(alphabet = 'a'; alphabet <= 'z'; alphabet++){
 		_putchar(alphabet);
