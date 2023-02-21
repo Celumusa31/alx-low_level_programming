6ebd0c2f10da28b29d914f5d248f34264793450e
Celumusa31 <celumusamthombeni31@gmail.com>
2023-02-21 10:01:55 -0800

    Task 1

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -9,3 +9,16 @@
  */
 
 int _putchar(char c);
+
+void print_alphabet(void);
+{
+	char alphabet;
+
+	for (alphabet = 'a' ;alphabet <= 'z' ;alphabet++)
+	{
+		_putchar(alphabet);
+	}
+	putchar('\n');
+	return (0);
+}
+
