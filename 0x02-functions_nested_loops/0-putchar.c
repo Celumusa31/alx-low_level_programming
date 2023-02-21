e4868ac3044d936369dfa681e977b955cc406408
Celumusa31 <celumusamthombeni31@gmail.com>
2023-02-21 07:05:35 -0800

    Editing task 0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,18 +1,16 @@
-#include <unistd.h>
-#include "main.h"
+#include <stdio.h>
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
+ * main - Entry point
  *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * Return: Always 0 (Success)
  */
-int _putchar(char c)
+int main(void)
 {
-	char c = _putchar
+	char c[] = "_putchar";
+
+	for (int = i = 0; i < sizeof(c); i++)
 	{
-		putchar(c);
+		putchar(c[i]);
 	}
-	putchar("\n");
 	return (0);
 }
