5f2c36a928b217c962769009759a787fd8461f73
Celumusa31 <celumusamthombeni31@gmail.com>
2023-02-21 12:58:44 -0800

    Task 3 updates

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,12 +1,14 @@
 #include "main.h"
 /**
- * print_islower - Entry point
+ * print _islower - Entry point
  *
  * Return: Always 1 (Success)
  * Return: Always 0 (Success) if otherwise
  */
 int _islower(int c)
 {
+	int c; 
+
 	if (c >= 'a' && c <= 'z')
 	{
 		return (1);
