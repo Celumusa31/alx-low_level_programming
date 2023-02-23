12f133a6500109e776fb048527aff7ddd2f5fde4
Celumusa31 <celumusamthombeni31@gmail.com>
2023-02-23 12:03:03 -0800

    Task 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_line - Entry point
+ *
+ * Return: Always void (Success)
+ */
+void print_line(int n)
+{
+	int i;
+	
+	for (i = 0; i < n; i++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
+
