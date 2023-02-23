fb99b8bce94c49ffcec519edfa96f8515af134d2
Celumusa31 <celumusamthombeni31@gmail.com>
2023-02-23 06:56:53 -0800

    Task 3

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * print_numbers - Entry point
+ *
+ * Return: Always void (Success)
+ */
+void print_numbers(void)
+{
+	for (i = 0; i <= 9; i++)
+	{
+		_putchar(i);
+	}
+	_putchar('\n');
+}
