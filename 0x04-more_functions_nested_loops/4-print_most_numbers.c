6421e5ae6f121f94bd0d9eb73df34d2bc53f6ad9
Celumusa31 <celumusamthombeni31@gmail.com>
2023-02-23 08:52:01 -0800

    Updating file

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -6,15 +6,17 @@
  */
 void print_most_numbers(void)
 {
-    int i;
+	int i;
 
-    for (i = 0; i <= 9; i++){
-        if (i == 2 || i == 4){
+ 	for (i = 0; i <= 9; i++)
+	{
+		if (i == 2 || i == 4)
+		{
 
-            continue;
-        }
+			continue;
+		}
 
-        putchar(i + '0');
-    }
-    putchar('\n')
+		_putchar(i + '0');
+	}
+	_putchar('\n');
 }
