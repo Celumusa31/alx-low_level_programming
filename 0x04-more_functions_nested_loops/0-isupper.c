b8fbcb9364764f5dc334e4d58d1284a379ec0911
Celumusa31 <celumusamthombeni31@gmail.com>
2023-02-23 04:20:43 -0800

    Task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * _isupper - Entry point
+ * @c: The character to be checked
+ * 
+ * Return: Always 1 or 0 (Success)
+ */
+int _isupper(int c)
+{
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	{
+	else
+	{
+		return (0);
+	}
+}
