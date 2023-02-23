cd1862b22884e319a3f275707ad0474663a68718
Celumusa31 <celumusamthombeni31@gmail.com>
2023-02-23 10:21:50 -0800

    Task 5 edits

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -7,12 +7,20 @@
 void more_numbers(void)
 {
 	int i;
-	int a;
+	int j;
+	int a = j / 10;
+	int b = j % 10;
 
 	for (i = 0; i < 10; i++)
-		for (a = 0; a <= 14; a++)
-		{
-			_putchar(a + '0');
-		}
-		_putchar('\n');
+		for (j = 0; j <= 14; j++)
+			if (j < 10)
+			{
+				_putchar(j + '0');
+			}
+			else
+			{
+				_putchar(a + '0');
+				_putchar(b + '0');
+			}
+	_putchar('\n');
 }
