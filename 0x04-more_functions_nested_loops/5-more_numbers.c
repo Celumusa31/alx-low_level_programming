f093fbb966bcb1c96a22bb6969430baee5a9be20
Celumusa31 <celumusamthombeni31@gmail.com>
2023-02-23 10:30:13 -0800

    Haibo

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,7 +10,9 @@ void more_numbers(void)
 	int j;
 
 	for (i = 0; i < 10; i++)
+	{
 		for (j = 0; j <= 14; j++)
+		{
 			if (j < 10)
 			{
 				_putchar(j + '0');
@@ -20,5 +22,7 @@ void more_numbers(void)
 				_putchar((j / 10) + '0');
 				_putchar((j % 10) + '0');
 			}
+		}
 		_putchar('\n');
+	}
 }
