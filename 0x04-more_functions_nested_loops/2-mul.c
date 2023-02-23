a7473b8e4b24f4587d95ef72bc792c9c8dd7f337
Celumusa31 <celumusamthombeni31@gmail.com>
2023-02-23 05:55:11 -0800

    Task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * mul - Entry point
+ * @a: The character to be checked
+ * @b: The character to be checked
+ *
+ * Return: Always 0 (Success)
+ */
+int mul(int a, int b)
+{
+	multiply = a * b;
+	{
+		_putchar(multiply);
+	}
+	return (0);
+}
