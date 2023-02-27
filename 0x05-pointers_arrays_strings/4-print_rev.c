367665636735afde868072c2d64e8ae815126076
Celumusa31 <celumusamthombeni31@gmail.com>
2023-02-27 08:57:46 -0800

    Task 4 edits

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,5 +1,7 @@
 #include "main.h"
 #include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 /**
  * print_rev - Entry point
  * @s: Character to be checked
@@ -8,21 +10,23 @@
  */
 void print_rev(char *s)
 {
-	int len;
-	int index;
+	unsigned int len;
+	unsigned int index;
 
 	char *start_ptr, *end_ptr, temp;
 
-	len = strlen(s);
+	len = sizeof(s);
 
 	start_ptr = s;
 	end_ptr = s + len - 1;
 
 	for (index = 0; index < len/2; index++)
+	{
 		temp = *end_ptr;
 		*end_ptr = *start_ptr;
 		*start_ptr = temp;
 
 		start_ptr++;
 		end_ptr--;
+	}
 }
