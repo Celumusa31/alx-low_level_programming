fdb849dd7e981de84a53ca68f2623c2200278b2a
Celumusa31 <celumusamthombeni31@gmail.com>
2023-02-27 10:11:57 -0800

    Task 5 updates

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -16,10 +16,10 @@ void rev_string(char *s)
 	begin = s;
 	end = s;
 
-	for ( c = 0 ; c < ( length - 1 ) ; c++ )
+	for (c = 0 ; c < (length - 1) ; c++)
 	end++;
 
-	for ( c = 0 ; c < length/2 ; c++ )
+	for (c = 0 ; c < length/2 ; c++)
 	{
 	temp = *end;
 	*end = *begin;
