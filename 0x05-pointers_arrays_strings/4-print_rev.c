fc8fd6a0cce85d2974d1a3765c55c45382ffd620
Celumusa31 <celumusamthombeni31@gmail.com>
2023-02-27 06:04:56 -0800

    Task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_rev - Entry point
+ * @s: Character to be checked
+ *
+ * Return: Always void (Success)
+ */
+void print_rev(char *s)
+{
+	revstr(s)
+}
