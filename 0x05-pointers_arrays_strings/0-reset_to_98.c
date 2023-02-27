5c4f620b4183059e10d7da10b9a14f266c200d53
Celumusa31 <celumusamthombeni31@gmail.com>
2023-02-26 23:16:10 -0800

    task 0

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - Entry point
+ * @n: The character to be checked
+ *
+ * Return: Always void (Success)
+ */
+void reset_to_98(int *n)
+{
+	int a;
+	int *n = &a;
+	*n = 98
+	{
+		_putchar(a);
+	}
+}
+
