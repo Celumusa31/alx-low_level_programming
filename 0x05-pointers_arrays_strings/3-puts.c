f4e9f2b62dad2f112d3e3ee48aa9f6c4ef085add
Celumusa31 <celumusamthombeni31@gmail.com>
2023-02-27 05:36:04 -0800

    Task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _puts - Entry point
+ * @str: Character to be checked
+ *
+ * Return: Always void (Success)
+ */
+void _puts(char *str)
+{
+	puts(str);
+}
+
