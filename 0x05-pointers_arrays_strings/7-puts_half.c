93fbc18d0fe4000e8d28992cf3a4533a1d029017
Celumusa31 <celumusamthombeni31@gmail.com>
2023-02-27 19:43:40 -0800

    Task 7

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * puts_half - Entry point
+ * *str: Character to be checked
+ * Return: Always void (Success)
+ */
+void puts_half(char *str)
+{
+	int j = 0;
+	int i;
+
+	while (str[j] != '\0')
+	{
+		j++
+	}
+	if (j % 2 == 1)
+	{
+		i = (j - 1) / 2;
+		i++;
+	}
+	else
+	{
+		i = j / 1;
+	}
+	for (j; i < j; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
