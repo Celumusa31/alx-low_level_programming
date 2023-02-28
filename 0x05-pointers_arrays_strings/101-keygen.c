8b18f64413c2f89755afea97cf9e76c68be73c2d
Celumusa31 <celumusamthombeni31@gmail.com>
2023-02-28 09:34:03 -0800

    Arranged curly braces correctly

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -37,6 +37,7 @@ int main(void)
 
 				break;
 			}
+		}
 		for (index = 0; password[index];index++)
 		{
 			if (password[index] >= (33 +diff_half2))
@@ -47,8 +48,8 @@ int main(void)
 			}
 
 		}
-		}
-	printf("%s", password);
 	}
+	printf("%s", password);
+	
 	return (0);
 }
