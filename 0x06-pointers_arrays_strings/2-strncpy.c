2ee0d0714145e77bd2f29c36d7b7899644ddb1bf
Celumusa31 <celumusamthombeni31@gmail.com>
2023-03-01 09:37:08 -0800

    Task 2 edits

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -11,12 +11,6 @@
  */
 char *_strncpy(char *dest, char *src, int n)
 {
-	strncpy(dest, src, size_t n);
-	size_t i;
-
-	for (i = 0; i < n && src[i] != '\0'; i++)
-		dest[i] = src[i];
-	for (; i < n, i++)
-		dest[i] = '\0';
+	strncpy(dest, src, size_t n)
 }
 
