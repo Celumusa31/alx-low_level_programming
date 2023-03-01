7b79b61d880d174c559a5ca7d9e3a882a641aa52
Celumusa31 <celumusamthombeni31@gmail.com>
2023-03-01 08:05:38 -0800

    Task 0

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * _strncpy - Entry point
+ * @dest: Character to be checked
+ * @src: Character to be checked
+ * @n: Character to be checked
+ *
+ * Return: Always char (Success)
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	strncpy(dest, src, n);
+}
+
---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * _strcmp - Entry point
+ * @s1: Character to be checked
+ * @s2: Character to be checked
+ *
+ * Return: Always integer (Success)
+ */
+int _strcmp(char *s1, char *s2)
+{
+	strcmp(s1, s2);
+}
