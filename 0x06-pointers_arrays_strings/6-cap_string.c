0e4da6520c2b025d692d022807751a496559ceba
Celumusa31 <celumusamthombeni31@gmail.com>
2023-03-02 07:15:34 -0800

    Task 6 updates

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -11,11 +11,12 @@ char *cap_string(char *str)
 {
 	int i;
 
-	for(i=0;i< i <= strlen(str); i++)
+	for (i = 0; i < i <= strlen(str); i++)
 	{
-		if(str[i] >= 97 && str[i] <= 122)
+		if (str[i] >= 97 && str[i] <= 122)
 		{
-			str[i] = str[i] -32;
+			str[i] = str[i] - 32;
 		}
 	}
+	return (str[i]);
 }
