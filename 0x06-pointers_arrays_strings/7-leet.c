8fcf483e68969647fe707d2b459d7ee5dad3181b
Celumusa31 <celumusamthombeni31@gmail.com>
2023-03-02 08:02:25 -0800

    Task 7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+  * leet - Entry point
+  * @str: Character to be checked
+  *
+  * Return: Always str (Success)
+  */
+char *leet(char *str)
+{
+	int a = 0, b = 0, l = 5;
+	char r[5] = {'A', 'E', 'O', 'T', 'L'};
+	char n[5] = {'4', '3', '0', '7', '1'};
+
+	while (str[a])
+	{
+		b = 0;
+
+		while (b < l)
+		{
+			if (str[a] == r[b] || str[a] - 32 == r[b])
+			{
+				str[a] = n[b];
+			}
+
+			b++;
+		}
+
+		a++;
+	}
+
+	return (str);
+}
