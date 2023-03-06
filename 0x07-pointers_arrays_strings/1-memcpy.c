754db41fa2c067757e0b166f5c105cf547fccb14
Celumusa31 <celumusamthombeni31@gmail.com>
2023-03-06 02:57:09 -0800

    Task 1

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * _memcpy - Entry point
+ * @dest: Character to be checked
+ * @src: Character to be checked
+ * @n: Character to be checked
+ *
+ * Return: Always pointer (Success)
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	memcpy(dest; src; sizeof(src) + n);
+	return (dest);
+}
