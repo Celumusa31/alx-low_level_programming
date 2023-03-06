640f4b24700943d8c6466bdde96abb07b82d9ff2
Celumusa31 <celumusamthombeni31@gmail.com>
2023-03-06 12:32:51 -0800

    Task 100

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * set_string - Entry point
+ * @s: Character to be checked
+ * @to: Character to be checked
+ *
+ * Return: Always void (Success)
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
