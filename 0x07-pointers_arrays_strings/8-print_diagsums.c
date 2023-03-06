57bad51f0c36b0b161a104e28483570a1d3e17a2
Celumusa31 <celumusamthombeni31@gmail.com>
2023-03-06 11:16:02 -0800

    Task 7 edits

--- 0x07-pointers_arrays_strings/8-print_diagsums.c
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -12,15 +12,8 @@ void print_diagsums(int *a, int size)
 {
 	int i, j;
 
-	sum = 0;
+	int sum = 0;
 
-	for (i = 0; i < size; i++)
-	{
-		for (j = 0; j < size; j++)
-		{
-			printf("%d", a[i][j]);
-		}
-	}
 	for (i = 0; i < size; i++)
 	{
 		for (j = 0; j < size; j++)
