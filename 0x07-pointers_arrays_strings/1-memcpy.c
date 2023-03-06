138fa81d422cf0b58b5f24caec16114694a1f88d
Celumusa31 <celumusamthombeni31@gmail.com>
2023-03-06 03:27:37 -0800

    Task 1

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -12,6 +12,13 @@
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	memcpy(dest; src; sizeof(src) + n);
+	int i;
+
+	char *src_char = (char *)src;
+	char *dest_char = (char *)dest;
+
+	for (i = 0; i < n; i++)
+		dest_char[i] = src_char[i];
+
 	return (dest);
 }
