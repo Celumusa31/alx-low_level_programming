75e16714dbc9693be8bce02dbec6bab0ecfd4b41
Celumusa31 <celumusamthombeni31@gmail.com>
2023-03-08 05:37:03 -0800

    Task 2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - Entry point
+ * @s: Character to be checked
+ *
+ * Return: Always int (Success)
+ */
+int _strlen_recursion(char *s)
+{
+	strlen(s);
+
+	return (s);
+}
