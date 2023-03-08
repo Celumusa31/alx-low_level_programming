c6b07d70c9008e05f3866e7f5a5e8195229ea3a5
Celumusa31 <celumusamthombeni31@gmail.com>
2023-03-08 06:56:35 -0800

    Task 4

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - Entry point
+ * @x: Character to be checked
+ * @y: Character to be checked
+ *
+ * Return: Always int (Success)
+ */
+int _pow_recursion(int x, int y)
+{
+	int answer = pow(x, y);
+	
+	if (y >= 0)
+		return (answer);
+
+	else (y < 0)
+		return (-1);
+}
