1ed0de44f59b9482d9c8ae714894ce549bd71335
Celumusa31 <celumusamthombeni31@gmail.com>
2023-03-08 09:57:52 -0800

    Task 6

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * is_prime_number - Entry point
+ * @n: Character to be checked
+ *
+ * Return: Always int (Success)
+ */
+int is_prime_number(int n)
+{
+	int i;
+
+	if (n == 0|| n == 1)
+		return (0);
+
+	if (n == i)
+		return (1);
+
+	if (n % i == 0)
+		return (0);
+
+	i++;
+	return (is_prime_number(n));
+}
+
