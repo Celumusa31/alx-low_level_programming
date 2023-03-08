3ce5be68b03892837accc488414e71bfc9da8248
Celumusa31 <celumusamthombeni31@gmail.com>
2023-03-08 01:55:25 -0800

    Task 1

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * _print_rev_recursion - Entry point
+ * @s: Character to be checked
+ *
+ * Return: Always void (Success)
+ */
+void _print_rev_recursion(char *s)
+{
+	strrev(s);
+}
