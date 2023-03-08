df12582ce508ee2e9b8886c597fd0562fe67ae9c
Celumusa31 <celumusamthombeni31@gmail.com>
2023-03-08 10:09:14 -0800

    Task 6 edits

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -7,18 +7,14 @@
  */
 int is_prime_number(int n)
 {
-	int i = 2;
-
-	if (n == 0|| n == 1)
-		return (0);
+	int i;
 
 	if (n == i)
 		return (1);
 
 	if (n % i == 0)
-		return (1);
+		return (0);
 
-	i++;
 	return (is_prime_number(n));
 }
 
