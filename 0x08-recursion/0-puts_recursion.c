a5d0af191999a65b940f7758fa3337efb7c37aee
Celumusa31 <celumusamthombeni31@gmail.com>
2023-03-08 01:42:30 -0800

    Task 0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _puts_recursion - Entry point
+ * @s: Character to be checked
+ *
+ * Return: Always void (Success)
+ */
+void _puts_recursion(char *s)
+{
+	puts(s);
+
+}
