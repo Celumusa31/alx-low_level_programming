3bbf9928f3cdf07bf415093e316949d1792bab88
Celumusa31 <celumusamthombeni31@gmail.com>
2023-03-08 06:29:41 -0800

    Editing task 3

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -8,8 +8,11 @@
  */
 int factorial(int n)
 {
-	if (n < 0)
-		return (-1);
+	if (n == 0)
+		return (1);
 
-	return n * factorial(n - 1);
+	return (n * factorial(n - 1));
+
+	else (n < 0)
+		return (-1);
 }
