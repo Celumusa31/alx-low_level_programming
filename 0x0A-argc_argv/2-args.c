c62e1f6caa44b965bbd0e3ab85782ca1708cdb32
Celumusa31 <celumusamthombeni31@gmail.com>
2023-03-11 13:07:14 -0800

    Task 2

---
+++ 0x0A-argc_argv/2-args.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @argc: Character to be checked
+ * @argcv: Character to be checked
+ *
+ * Return: Always integer (Success)
+ */
+int main(int argc, char *argv[])
+{
+	int i;
+
+	for (i = 0; i < argc; i++)
+	{
+		printf("%s\n", argv[i]);
+	}
+	return (0);
+}
+
