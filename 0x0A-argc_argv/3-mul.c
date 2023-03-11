7824e3d8025a496abe33f9a8c5f8692e26b31109
Celumusa31 <celumusamthombeni31@gmail.com>
2023-03-11 13:52:04 -0800

    Task 3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Entry point
+ * @argc: Character to be checked
+ * @argv: Character to be checked
+ *
+ * Return: Always integer (Success)
+ */
+int main(int argc, char *argv[])
+{
+	int result;
+	int num1;
+	int num2;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+
+	result = num1 * num2;
+	printf("%d\n", result);
+
+	return (0);
+}
