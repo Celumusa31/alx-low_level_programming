b6d80a9e0d451562bce1af592275c142adbbb956
Celumusa31 <celumusamthombeni31@gmail.com>
2023-03-13 19:27:49 -0700

    Task 0

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * create_array - Entry point
+ * @size; Character to be checked
+ * @c: Character to be checked
+ *
+ * Return: Alwyas pointer (Success)
+ */
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i;
+	char *ptr;
+
+	if (size == 0)
+		return (NULL);
+
+	ptr = malloc(size * sizeof(char));
+
+	if (ptr == NULL)
+		return (NULL);
+
+	for (i = 0; i < size; i++)
+	{
+		ptr[i] = c;
+	}
+
+	return (ptr);
+}
