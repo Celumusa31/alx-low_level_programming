f3cf63fb6732eb4084a9ab3355b1d679ae566752
Celumusa31 <celumusamthombeni31@gmail.com>
2023-03-20 07:53:27 -0700

    Task 2

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,33 @@
+#include "dog.h"
+#include <stdio.h>
+
+/**
+ * print_dog - Entry point
+ * @d: Charcater to be checked
+ * 
+ * Return: Awlays void (Success)
+ */
+void print_dog(struct dog *d)
+{
+	char *name = d->name;
+	float age = d->age;
+	char *owner = d->owner;
+
+	if (name == NULL)
+		printf("Name: (nil)\n");
+	else
+		printf("Name: %s\n", d->name);
+
+	if (age <= 0)
+		printf("Age: (nil)\n");
+	else
+                printf("Age: %f\n", d->age);
+
+	if (owner == NULL)
+		printf("Owner: (nil)\n");
+	else
+		printf("Owner: %s\n", d->owner);
+
+	if (d == NULL)
+		printf("%s", "");
+}
