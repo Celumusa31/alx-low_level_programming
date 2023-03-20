08d59330184229a33be9204265575171c86d6947
Celumusa31 <celumusamthombeni31@gmail.com>
2023-03-20 06:53:25 -0700

    Task 1

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+#include "dog.h"
+
+/**
+ * init_dog - Entry point
+ * @d: Characater to be checked
+ * @name: Character to be checked
+ * @age: Character tot be checked
+ * @owner: Character to be checked
+ *
+ * Return: Always void (Success)
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d.name = name;
+	d.age = age;
+	d.owner = owner;
+}
