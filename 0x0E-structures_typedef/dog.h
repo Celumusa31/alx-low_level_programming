a9b7b5a766468854d9920579489d5a004b542955
Celumusa31 <celumusamthombeni31@gmail.com>
2023-03-20 06:00:46 -0700

    Task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,18 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Entry point
+ * @name: Character to be checked
+ * @age: Character to be checked
+ * @owner: Character to be checked
+ *
+ * Return: Always void (Success)
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+#endif
