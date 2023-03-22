dda7fa5da3b58b557933f10c7f6c507b5efeaf0f
Celumusa31 <celumusamthombeni31@gmail.com>
2023-03-22 11:58:31 -0700

    Task 0

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -3,7 +3,7 @@
 /**
  * print_name - Entyr point
  * @name: Charceter to be checked
- *
+ * @f: Character to be checked
  * Return: Always void (Success)
  */
 void print_name(char *name, void (*f)(char *))
