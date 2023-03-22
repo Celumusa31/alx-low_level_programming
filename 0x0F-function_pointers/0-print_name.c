feaabd43f0423ef6b81ede3e93c14d180117690a
Celumusa31 <celumusamthombeni31@gmail.com>
2023-03-22 12:01:00 -0700

    Task 0 edits

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,9 +1,10 @@
 #include "function_pointers.h"
 
 /**
- * print_name - Entyr point
+ * print_name - Entry point
  * @name: Charceter to be checked
- * @f: Character to be checked
+ * @f: pointer function
+ *
  * Return: Always void (Success)
  */
 void print_name(char *name, void (*f)(char *))
