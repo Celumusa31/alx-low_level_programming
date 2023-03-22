ca881537e30d789f9ce510282f7a996e0e04a6d5
Celumusa31 <celumusamthombeni31@gmail.com>
2023-03-22 12:14:15 -0700

    Task 0 edits

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,3 +1,5 @@
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTER_H
 #include <unistd.h>
 
 /**
@@ -10,3 +12,5 @@
 int _putchar(char c);
 
 void print_name(char *name, void (*f)(char *));
+
+#endif
