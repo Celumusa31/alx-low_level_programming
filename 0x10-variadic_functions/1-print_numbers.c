71d4b2ca1c83195f757f874abc9a06b125660dc6
Celumusa31 <celumusamthombeni31@gmail.com>
2023-03-23 09:09:45 -0700

    Moved declaration of i

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -11,11 +11,11 @@
  */
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
+	unsigned int i;
+
 	va_list variable_list;
 
 	va_start(variable_list, n);
-
-	unsigned int i;
 
 	for (i = 0; i < n; i++)
 	{
