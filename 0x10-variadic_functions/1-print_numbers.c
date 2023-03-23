501c34fc39a865b57e65b082d48c000f2018b4f6
Celumusa31 <celumusamthombeni31@gmail.com>
2023-03-23 08:57:35 -0700

    task 1

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,35 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * print_numbers - Entry point
+ * @saparator: Character to be checked
+ * @n: Character to be checked
+ *
+ * Return: Always void (Success)
+ */
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	va_list variable_list;
+
+	va_start(variable_list, n);
+
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		if (i != n - 1)
+		{
+			printf("%d%s", va_arg(variable_list, int), separator);
+		}
+		else
+		{
+			printf("%d\n", va_arg(variable_list, int));
+		}
+
+	}
+
+	va_end(variable_list);
+	printf("\n");
+}
