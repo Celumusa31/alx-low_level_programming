9c0de8fda4365da8acdbcb83c7b7bdb891d4e8ac
Celumusa31 <celumusamthombeni31@gmail.com>
2023-04-06 12:02:34 -0700

    task 0

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,23 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <sys/uio.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c);
+
+ssize_t read_textfile(const char *filename, size_t letters);
+#endif
