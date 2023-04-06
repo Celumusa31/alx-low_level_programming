4641f1b4be3513356e3f619d99c9fd652362fd3a
Celumusa31 <celumusamthombeni31@gmail.com>
2023-04-06 12:35:14 -0700

    Task 2

---
+++ 0x15-file_io/2-append_text_to_file.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * append_text_to_file - Entry point
+ * @filename: Character to be checked
+ * @text_content: Character to be checked
+ *
+ * Return: Always int (Success)
+ */
+
+int append_text_to_file(const char *filename, char *text_content)
+{
+	int fp, writing;
+	int length = 0;
+
+	if (filename == NULL)
+	{
+		return (-1);
+	}
+	if (text_content != NULL)
+	{
+		for (length = 0; text_content[length])
+			length++;
+	}
+
+	fp = open(filename, O_WRONLY | O_APPEND);
+	writing = write(fp, text_content, length);
+
+	if (fp == -1 || writing == -1)
+		return (-1);
+
+	close(fp);
+
+	return (1);
+}
