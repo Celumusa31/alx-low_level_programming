f1ddfecaf1401c2f14d7abab9159c80406f5bbc4
Celumusa31 <celumusamthombeni31@gmail.com>
2023-04-10 10:00:54 -0700

    Task 3 edits

--- 0x14-bit_manipulation/3-set_bit.c
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 /**
- * se_bit - Entry point
+ * set_bit - Entry point
  * @n: Character to be checked
  * @index: Character to be checked
  * Return: Always int (Success)
