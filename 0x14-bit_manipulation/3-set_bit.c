1cf05ba8d26a131dd6c87277728d7bf6fd5f4099
Celumusa31 <celumusamthombeni31@gmail.com>
2023-05-05 04:52:06 -0700

    Task 3

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * set_bit - Entry point
+ * @n: Character to be checked
+ * @index: Character to be checked
+ * Return: Always int (Success)
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned int i = (sizeof(unsigned long int) * 8);
+
+	if (index >= i)
+	{
+		return (-1);
+	}
+	*n |= (1 << index);
+	return (1);
+}
